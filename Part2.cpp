983b92e122dcc298e8d730136d71f1d799b52cf3
Pranjal Gupta <pranjalgupta2199@gmail.com>
2018-10-24 04:08:49 +0530

    Adds comments to proof checking engine

--- Part2.cpp
+++ Part2.cpp
@@ -15,6 +15,7 @@ using namespace std;
 	(<statement> → <statement>)
 	<rule> ::= ∧i | ∧e1 | ∧e2 | ∨i1 | ∨i2 | →e |P
 
+	Algorithm : 
 	Make a object (statement) with value, rule, line1, line2 and its truth value
 	
 	Assign each premise a truth value : 
@@ -24,7 +25,7 @@ using namespace std;
 	Then check iteratively, whether any non-premise ith statement (2 <= i <= n) 
 	can be derived from any of it's previous statements.
 		1. If yes, upadate the truth value of that Statement object.
-			> In case of AND introduction, both the premises must be present.
+			> In case of AND introduction, both the premises must be present and truth val must be true.
 			> In case of AND elimination, a new premise object will be created.
 			> In case of OR introduction, first check if either of one them is true, then make another
 				premise object and assign T as it's truth value.
@@ -38,6 +39,25 @@ using namespace std;
 
 
 class Statement{
+/*
+	This is a class for all the statements that you enter in 
+	the proof.
+	
+	Data Members : 
+		value : string
+			The formula which is either a premise or a statement 
+			derived from other statements
+		rule : string
+			The rule used to derive the value (data member) of the 
+			statement. Allowed values are given in the definitions
+		line1 : int
+			The first line number of the statement in the proof from which 
+			this statement is being derived
+		line2 : int
+			The second line number of the statement in the proof from which 
+			this statement is being derived
+			
+*/
 
 public :
 	string value;
@@ -45,15 +65,29 @@ public :
 	int line1 = 0;
 	int line2 = 0;
 	
-	Statement(string s) {split(s);}
-
+	Statement(string s) {
+	/*
+		Constructor of Statement class
+		Intialises all the data members
+	*/
+		split(s);
+	}
+		
 
 	void split(string s) {
+	/*
+		Splits the param and assigns value to the data members of the object
+		Param : 
+			s : string
+			 	A line in the proof rules
+	*/
 		int counter = 0;
 		string temp;
 		for (int i = 0; i < s.length(); i++){
-			if (s[i] != '/') temp += s[i];
+		// add the character while '/' is not encountered
+			if (s[i] != '/') temp += s[i]; 
 			else {
+			// assign values to data members
 				assign(counter, temp);
 				counter ++;
 				temp = s[i+1];
@@ -61,10 +95,15 @@ public :
 			}
 		}
 		assign(counter, temp);	
+		// assign value to remaining string
 	}
 	
-	void assign(int counter, string temp)
-	{
+	void assign(int counter, string temp){
+	/*
+		Input is of the form : 
+			<statement>/<rule>[ /line1[ /line2 ]]
+counter: 		0		 1 		 2			3
+	*/
 		if (counter == 0) value = temp;
 		else if (counter == 1) rule = temp;
 		else if (counter == 2) line1 = stoi(temp) - 1;
@@ -72,6 +111,9 @@ public :
 	}
 	
 	void print(){
+	/*
+		Prints all the values of the data members
+	*/
 		cout << value << " " << rule << " " <<  line1 << " " << line2 << endl;
 	}
 	
@@ -86,9 +128,13 @@ bool and_intro(vector <Statement *> proof, vector <bool> truth_val, int index){
 	string rule1 = proof[line1]->value;
 	string rule2 = proof[line2]->value;
 	
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2
 	if (formula->value != "(" + rule1 + "^" + rule2 + ")") return false;
+	// Then check the truth value of those statements. For AND introduction, 
+	// both the statements have to be true
 	else if (truth_val[line1] == true && truth_val[line2] == true) return true;
-	else return true;
+	else return false;
 }
 
 
@@ -96,10 +142,15 @@ bool and_el1(vector <Statement *> proof, vector <bool> truth_val, int index){
 	Statement * formula = proof[index];
 	
 	int l1 = formula->line1;
-	string sub = proof[l1]->value;
 	int n1 = formula->value.length() + 2;
-	
+
+	string sub = proof[l1]->value;
+
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2	
 	if ("(" + formula->value + "^" + sub.substr(n1) != sub) return false;
+	// Then check the truth value of statement at line1. If it's true then,
+	// and of formula and line1 statement is true. return true
 	else if (truth_val[l1] == true) return true;
 	else return false;
 }
@@ -107,10 +158,15 @@ bool and_el1(vector <Statement *> proof, vector <bool> truth_val, int index){
 bool and_el2(vector <Statement *> proof, vector <bool> truth_val, int index){
 	Statement * formula = proof[index]; 
 	int l1 = formula->line1; 
+	
 	string sub = proof[l1]->value; 
 	int n1 = sub.length() - 2 - formula->value.length();
-	
-	if (sub.substr(0,n1) + "^" + formula->value + ")" != sub) return false; 
+
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2		
+	if (sub.substr(0,n1) + "^" + formula->value + ")" != sub) return false;
+	// Then check the truth value of statement at line1. If it's true then,
+	// and of formula and line1 statement is true. return true
 	else if (truth_val[l1] == true) return true;
 	else return false;
 }
@@ -120,8 +176,12 @@ bool or_intro1(vector <Statement *> proof, vector <bool> truth_val, int index) {
 	
 	string sub = proof[l1]->value; 
 	int n = sub.length() ; 
-	
+
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2
 	if ("(" + sub + "V" + formula->value.substr(n+2) != formula->value) return false;
+	// Then check the truth value of statement at line1. If it's true then,
+	// or of formula and line1 statement is true. return true
 	else if (truth_val[l1] == true) return true;
 	else return false;
 
@@ -133,8 +193,12 @@ bool or_intro2(vector <Statement *> proof, vector <bool> truth_val, int index) {
 	int f_length = formula->value.length();
 	string sub = proof[l1]->value; 
 	int n = sub.length() ; 
-	
+
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2	
 	if ("V" + sub + ")" != formula->value.substr(f_length - n -2)) return false;
+	// Then check the truth value of statement at line1. If it's true then,
+	// or of formula and line1 statement is true. return true
 	else if (truth_val[l1] == true) return true;
 	else return false;
 
@@ -143,8 +207,12 @@ bool impl_el(vector <Statement *> proof, vector <bool> truth_val, int index) {
 	Statement * formula = proof[index];
 	int l1 = formula->line1;
 	int l2 = formula->line2;
-	
+
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2	
 	if ("(" + proof[l2]->value + "->" + formula->value + ")" != proof[l1]->value) return false;
+	// if the implication and the antecedent both are true, then the 
+	// consequent is also true
 	else if (truth_val[l1] == true && truth_val[l2] == true) return true;
 	else return false;
 }
@@ -152,7 +220,12 @@ bool MT(vector <Statement *> proof, vector <bool> truth_val, int index) {
 	Statement * formula = proof[index];
 	int l1 = formula->line1;
 	int l2 = formula->line2;
+	
+	// checks whether the statement can be derived from other statement 
+	// at line1 and line2	
 	if ("(" + formula->value.substr(1) + "->" + proof[l2]->value.substr(1) + ")" != proof[l1]->value) return false;
+	// Then check if the implication is true and negation of consequent is true
+	// then negation of antecedent must be true.
 	else if (truth_val[l1] == true && truth_val[l2] == true) return true;
 	else return false;
 }
@@ -161,7 +234,9 @@ bool MT(vector <Statement *> proof, vector <bool> truth_val, int index) {
 
 
 bool assign_rule(string rule,vector <Statement *> proof, vector <bool> truth_val, int index){
-	
+	/*
+		Check for which rule is being used and call that function
+	*/
 	if (rule == "P") return true;
 	else if (rule == "^i") return and_intro(proof, truth_val, index);
 	else if (rule == "^e1") return and_el1(proof, truth_val, index);
@@ -170,10 +245,18 @@ bool assign_rule(string rule,vector <Statement *> proof, vector <bool> truth_val
 	else if (rule == "Vi2") return or_intro2(proof, truth_val, index);
 	else if (rule == "->e") return impl_el(proof, truth_val, index);
 	else if (rule == "MT") 	return MT(proof, truth_val, index);
-	else return false;
+	else return false; // if no matching rules, return false
 }	
 
 void check_validity(vector <Statement *> proof, vector <bool> truth_val){
+	/*
+		Iterate through all the statements and check their truth value.
+		If anyone of them becomes false after evaluation from its constituents,
+		then the proof becomes INVALID.
+		Else the proof is VALID, if there is no statement which becomes
+		false after evaluation from its constituents 
+	*/
+	
 	for (int i = 0; i < proof.size(); i++)
 	{
 		Statement *temp;
@@ -189,12 +272,15 @@ void check_validity(vector <Statement *> proof, vector <bool> truth_val){
 	cout << "VALID PROOF" << endl;
 }
 
-int main(){
-	int n;
-	string s;
-	Statement *t;
+void prompt(){
+/*
+	Calls other functions of proof rules and validity check
+*/
+	int n; // number of lines in the proof
+	string s; // string containing the input
+	Statement *t; // pointer to statement object
 	
-	vector <Statement *> proof;
+	vector <Statement *> proof; 
 	vector <bool> truth_val;
 	
 	cin >> n;
@@ -204,44 +290,18 @@ int main(){
 		
 		proof.push_back(t);
 		
+		// assign truth_val as 'true' in case of premise 
+		// else 'false' for all other statements
 		if (t -> rule == "P") truth_val.push_back(true);
 		else truth_val.push_back(false);
 	}
 	
+	// check the validity of the proof
 	check_validity(proof, truth_val);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(){
+	prompt();
+}
 
 
